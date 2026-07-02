d421070c0771ca43d7f8a4d933cab6ebbc013816
blackstar19920311 <blackstar19920311@users.noreply.github.com>
2026-07-02 11:18:25 +0200

    Remove test text from version string

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 const pad = (n) => String(n).padStart(2, '0')
 const now = new Date()
-const version = `v${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())} (CI/CD Automatikus Frissítés)`
+const version = `v${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}`
 
 export default defineConfig({
   define: {
